f19ab33406e8e4cf199ae1580580015de0499fc5
Marley <marley@workstation.marley.local>
2013-07-09 13:40:43 +0400

    first commit

---
+++ src/main/java/com/marakana/contracts/entities/Contact.java
@@ -0,0 +1,37 @@
+package com.marakana.contracts.entities;
+
+public class Contact {
+	
+	private Long id;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	// ------------------
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	// ------------------	
+	
+	private Long addressId;
+	
+	public Long getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(Long addressId) {
+		this.addressId = addressId;
+	}
+
+}
---
+++ src/main/java/com/marakana/contracts/repositories/AddressRepository.java
@@ -0,0 +1,48 @@
+package com.marakana.contracts.repositories;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import com.marakana.contracts.entities.Address;
+
+public class AddressRepository {
+	
+	private final DataSource ds;
+	
+	public AddressRepository(){
+		
+		try {
+			Context context = new InitialContext();
+			
+			try {
+				ds = (DataSource) context.lookup("java:comp/env/jdbc/trainingdb");
+			} finally {
+				context.close();
+			}
+		} catch (NamingException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+
+	public void init(){
+		
+		
+	}
+		
+	public Address find(long id){
+		return null; // TODO
+		
+	}
+	
+	public void create(Address address){
+		
+	}
+	
+	public void delete(Address address){
+		
+	}
+	
+}
